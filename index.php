3e076705de8e0b588258bfdd70327200173324fd
Nicolas De Palma <wellcomenicolas@gmail.com>
2022-05-25 15:14:24 +0200

    Definito un costruttore

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+include __DIR__ . '/db.php';
+
+include __DIR__ . '/layout/head.php'; ?>
+
+<div class="container">
+    <?php foreach ($movies as $movie) : ?>
+        <div class="card">
+            <img class="img-fluid" src="<?= $movie['poster_path'] ?>" alt="<?= $movie['title'] ?>">
+            <div class="card-body">
+                <h4><?= $movie['title']; ?> </h4>
+                <p><?= $movie['overview']; ?> </p>
+            </div>
+            <?php endforeach; ?>
+        </div>
+</div>
+
+
+<?php include __DIR__ . '/layout/footer.php' ?>
+
+
+
