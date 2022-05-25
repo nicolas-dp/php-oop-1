e13c5aa670014997c34c64d1c00c948712edacb1
Nicolas De Palma <wellcomenicolas@gmail.com>
2022-05-25 16:07:30 +0200

    Stampa a schermo delle card

--- Models/Movies.php
+++ Models/Movies.php
@@ -15,23 +15,18 @@ class Movie
         $this->poster_path = $poster_path;
         $this->title = $title;
     }
+
+
 }
 
-$movie_x = new Movie(
-    1,
-    "en",
-    "The characters we met a little more than a decade ago return to East Great Falls for their high-school reunion. In one long-overdue weekend, they will discover what has changed, who hasn’t and that time and distance can’t break the bonds of friendship.",
-    "https://image.tmdb.org/t/p/w342/de5QBIdVR4dnkBZ4a0zjkS4lTg.jpg",
-    "American Reunion"
-);
-
-$movie_y = new Movie(
-    2,
-    "en",
-    "Erik, Ryan, and Cooze start college and pledge the Beta House fraternity, presided over by none other than legendary Dwight Stifler. But chaos ensues when a fraternity of geeks threatens to stop the debauchery and the Betas have to make a stand for their right to party.",
-    "https://image.tmdb.org/t/p/w342/cEJMqmCGdKJkmjWwi3Iv6l4kMIK.jpg",
-    "American Pie Presents: Beta House"
-);
 
 
-var_dump($movie_x, $movie_y);
+$movies = [
+
+new Movie(1, "en", "The characters we met a little more than a decade ago return to East Great Falls for their high-school reunion. In one long-overdue weekend, they will discover what has changed, who hasn’t and that time and distance can’t break the bonds of friendship.", "https://image.tmdb.org/t/p/w342/de5QBIdVR4dnkBZ4a0zjkS4lTg.jpg", "American Reunion"),
+
+new Movie(2, "en", "Erik, Ryan, and Cooze start college and pledge the Beta House fraternity, presided over by none other than legendary Dwight Stifler. But chaos ensues when a fraternity of geeks threatens to stop the debauchery and the Betas have to make a stand for their right to party.", "https://image.tmdb.org/t/p/w342/cEJMqmCGdKJkmjWwi3Iv6l4kMIK.jpg", "American Pie Presents: Beta House")
+
+];
+
+//var_dump($movies);
--- index.php
+++ index.php
@@ -7,10 +7,10 @@ include __DIR__ . '/layout/head.php'; ?>
 <div class="container">
     <?php foreach ($movies as $movie) : ?>
         <div class="card">
-            <img class="img-fluid" src="<?= $movie['poster_path'] ?>" alt="<?= $movie['title'] ?>">
+            <img class="img-fluid" src="<?= $movie->poster_path ?>" alt="<?= $movie -> title ?>">
             <div class="card-body">
-                <h4><?= $movie['title']; ?> </h4>
-                <p><?= $movie['overview']; ?> </p>
+                <h4><?= $movie -> title; ?> </h4>
+                <p><?= $movie -> overview; ?> </p>
             </div>
             <?php endforeach; ?>
         </div>
