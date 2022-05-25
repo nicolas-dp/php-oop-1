c38a752ed8ba207186f8e8fe4683f0e57e908ee3
Nicolas De Palma <wellcomenicolas@gmail.com>
2022-05-25 16:50:01 +0200

    Inserimento metodo nell'oggetto

--- Models/Movies.php
+++ Models/Movies.php
@@ -16,17 +16,22 @@ class Movie
         $this->title = $title;
     }
 
-
+    function saluto(){
+        echo "Benvenuto nella pagina di American PIE";
+     }    
 }
 
 
 
 $movies = [
 
-new Movie(1, "en", "The characters we met a little more than a decade ago return to East Great Falls for their high-school reunion. In one long-overdue weekend, they will discover what has changed, who hasn’t and that time and distance can’t break the bonds of friendship.", "https://image.tmdb.org/t/p/w342/de5QBIdVR4dnkBZ4a0zjkS4lTg.jpg", "American Reunion"),
-
-new Movie(2, "en", "Erik, Ryan, and Cooze start college and pledge the Beta House fraternity, presided over by none other than legendary Dwight Stifler. But chaos ensues when a fraternity of geeks threatens to stop the debauchery and the Betas have to make a stand for their right to party.", "https://image.tmdb.org/t/p/w342/cEJMqmCGdKJkmjWwi3Iv6l4kMIK.jpg", "American Pie Presents: Beta House")
+    new Movie(1, "en", "The characters we met a little more than a decade ago return to East Great Falls for their high-school reunion. In one long-overdue weekend, they will discover what has changed, who hasn’t and that time and distance can’t break the bonds of friendship.", "https://image.tmdb.org/t/p/w342/de5QBIdVR4dnkBZ4a0zjkS4lTg.jpg", 'American Reunion'),
+    new Movie(2, "en", "Erik, Ryan, and Cooze start college and pledge the Beta House fraternity, presided over by none other than legendary Dwight Stifler. But chaos ensues when a fraternity of geeks threatens to stop the debauchery and the Betas have to make a stand for their right to party.", "https://image.tmdb.org/t/p/w342/cEJMqmCGdKJkmjWwi3Iv6l4kMIK.jpg", "American Pie Presents: Beta House")
 
 ];
+
+/* 
+$saluto = new Movie; 
+$saluto -> saluto(); */
 
 //var_dump($movies);
--- index.php
+++ index.php
@@ -1,23 +1,20 @@
 <?php
 
 include __DIR__ . '/db.php';
-
 include __DIR__ . '/layout/head.php'; ?>
 
 <div class="container">
+<div class="row row-cols-1 row-cols-sm-2 row-cols-md-6 justify-content-center g-4">
     <?php foreach ($movies as $movie) : ?>
-        <div class="card">
-            <img class="img-fluid" src="<?= $movie->poster_path ?>" alt="<?= $movie -> title ?>">
+        <div class="col p-3">
+            <img class="img-fluid" src="<?= $movie->poster_path ?>" alt="<?= $movie->title ?>">
             <div class="card-body">
-                <h4><?= $movie -> title; ?> </h4>
-                <p><?= $movie -> overview; ?> </p>
+                <h4><?= $movie->title; ?> </h4>
+                <p><?= $movie->overview; ?> </p>
             </div>
-            <?php endforeach; ?>
         </div>
+    <?php endforeach; ?>
 </div>
 
 
 <?php include __DIR__ . '/layout/footer.php' ?>
-
-
-
